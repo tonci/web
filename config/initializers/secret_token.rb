84205f1f3d5e673e409532469510e7d4261488b7
Tonci <t_o_n_c_i@abv.bg>
2012-02-17 13:33:53 -0800

    start rails with some 2-3 scaffolds

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Web::Application.config.secret_token = '998214ab5541b2a66d3d8c4cb43010b68fdc723ab2bce30c49f0c1005e9fdb9f9bd67d5843f64e378b6a339b30f30e2ffca7a6b57b35679fe50d6404dfd72d72'
